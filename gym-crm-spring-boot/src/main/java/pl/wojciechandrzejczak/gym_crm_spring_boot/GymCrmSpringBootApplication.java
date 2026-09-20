f6cc5d2da68a6798fea78886d8170a9724f61b05
Buumty <wojtekand22@gmail.com>
2026-09-20 13:17:17 +0200

    Initial commmit

---
+++ gym-crm-spring-boot/src/main/java/pl/wojciechandrzejczak/gym_crm_spring_boot/GymCrmSpringBootApplication.java
@@ -0,0 +1,13 @@
+package pl.wojciechandrzejczak.gym_crm_spring_boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GymCrmSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GymCrmSpringBootApplication.class, args);
+	}
+
+}
---
+++ gym-crm-spring-boot/src/test/java/pl/wojciechandrzejczak/gym_crm_spring_boot/GymCrmSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.wojciechandrzejczak.gym_crm_spring_boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GymCrmSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
